bbd8ccf371c21c574842b943737da57aab849da7
patrick ogutu <patywafula2019@gmail.com>
2026-08-19 17:12:51 +0300

    Update sitemap hostname

--- project/server.ts
+++ project/server.ts
@@ -199,7 +199,7 @@ Allow: /
 Disallow: /portal/
 Disallow: /dashboard
 
-Sitemap: https://assistbridge.online/sitemap.xml`);
+  Sitemap: https://boffinglobalgroup.com/sitemap.xml`);
   });
 
   app.get('/sitemap.xml', (req, res) => {
@@ -224,7 +224,7 @@ Sitemap: https://assistbridge.online/sitemap.xml`);
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   ${pages.map(page => `
   <url>
-    <loc>https://assistbridge.online${page}</loc>
+    <loc>https://boffinglobalgroup.com${page}</loc>
     <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>${page === '/' ? '1.0' : '0.8'}</priority>
